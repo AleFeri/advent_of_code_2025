cbdb6b661ea7df6458f359fa0489d955e32bc00a
AleFeri <50620354+AleFeri@users.noreply.github.com>
2025-12-02 11:45:05 +0100

    Completed day 2 part 1 & 2

---
+++ d02/part1.rs
@@ -0,0 +1,34 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let file_path = "input.txt";
+
+    let input = read_line(file_path);
+
+    println!("Input: {}", input);
+    let mut result: i128 = 0;
+    for id_range in input.split(',') {
+        let id1: i128 = id_range.split('-').next().unwrap().parse().unwrap();
+        let id2: i128 = id_range.split('-').nth(1).unwrap().parse().unwrap();
+
+        for id in id1..=id2 {
+            if has_repetitions(id) {
+                result += id;
+            }
+        }
+    }
+    println!("Result: {}", result);
+}
+
+fn has_repetitions(id: i128) -> bool {
+    let sid: &str = &id.to_string();
+    let len: usize = sid.len();
+    if len % 2 != 0 {
+        return false;
+    }
+    return sid[..len/2].contains(&sid[len/2..]);
+}
+
+fn read_line(filename: &str) -> String {
+    String::from(read_to_string(filename).unwrap().clone().strip_suffix('\n').unwrap())
+}
